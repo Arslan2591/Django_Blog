4cbb0e4551bc660151a8f234ae4279463267058e
Arslan Syed <arslan2591@gmail.com>
2020-08-20 02:40:09 +0500

    replaced dumy data with data from instance of models/database

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Arslan Syed',
-        'title': 'Why I love Movies',
-        'content': 'First Post Content',
-        'date_posted': 'August 19, 2020',
-    },
-    {
-        'author': 'Sidra',
-        'title': 'Why I hate Movies',
-        'content': 'Second Post Content',
-        'date_posted': 'August 18, 2020',
-    }
-]
+# posts = [
+#     {
+#         'author': 'Arslan Syed',
+#         'title': 'Why I love Movies',
+#         'content': 'First Post Content',
+#         'date_posted': 'August 19, 2020',
+#     },
+#     {
+#         'author': 'Sidra',
+#         'title': 'Why I hate Movies',
+#         'content': 'Second Post Content',
+#         'date_posted': 'August 18, 2020',
+#     }
+# ]
 
 
 def home(request):
 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
